f1579425bf21247cb53ba9c0f96e7b0239f95ba9
katgzco <katgzco@gmail.com>
2020-09-24 00:22:13 -0500

    Fix the lines in minimun

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,43 +5,39 @@
  */
 void print_times_table(int n)
 {
-	if (n <= 15 && n >= 0)
-	{
-		int n1, n2, segnum, resul;
+	/* logica booleana ley de morgan */
+	if (n > 15 || n < 0)
+		return;
+	int n1, n2, segnum, resul;
 
-		for (n1 = 0; n1 <= n; n1++)
+	for (n1 = 0; n1 <= n; n1++)
+	{
+		_putchar('0');
+		for (n2 = 1; n2 <= n; n2++)
 		{
-			for (n2 = 0; n2 <= n; n2++)
+			resul = n1 * n2;
+			_putchar(',');
+			_putchar(' ');
+			if (resul <= 9)
 			{
-				resul = n1 * n2;
-				if (n2 == 0)
-				{
-					_putchar(resul + '0');
-				}
-				_putchar(',');
 				_putchar(' ');
-				if (resul <= 9)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(resul + '0');
-				}
-				else if (resul <= 99)
-				{
-					_putchar(' ');
-					_putchar(resul / 10 + '0');
-					_putchar(resul % 10 + '0');
-				}
-				else
-				{
-					segnum = resul / 10;
-					segnum = segnum % 10;
-					_putchar(resul / 100 + '0');
-					_putchar(segnum + '0');
-					_putchar(resul % 10 + '0');
-				}
+				_putchar(' ');
+				_putchar(resul + '0');
 			}
-			_putchar('\n');
+			else if (resul <= 99)
+			{
+				_putchar(' ');
+				_putchar(resul / 10 + '0');
+				_putchar(resul % 10 + '0');
+			}
+			else
+			{
+				segnum =((resul / 10) % 10);
+				_putchar(resul / 100 + '0');
+				_putchar(segnum + '0');
+				_putchar(resul % 10 + '0');
+			}
 		}
+		_putchar('\n');
 	}
 }
