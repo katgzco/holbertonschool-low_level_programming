cf9f0e04f821e9483d18cc9a4080e1f09c48802b
katgzco <katgzco@gmail.com>
2020-09-25 16:23:14 -0500

    advance

---
+++ 0x04-more_functions_nested_loops/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Print all possible combination of two-digit numbers.
+ * Return: 0 always sucesull.
+ */
+int main(void)
+{
+	int i, j;
+
+	for (j = 0; j <= 98; j++)
+	{
+		for (i = j; i <= 99; i++)
+		{
+			if (i != j)
+			{
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				putchar(' ');
+
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				if (j < 98)
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
