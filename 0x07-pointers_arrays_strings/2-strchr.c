7eb4aadb0f40cabd105ded0295213bb27c873400
katgzco <katgzco@gmail.com>
2020-10-05 19:42:55 -0500

    Fix return

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,10 +7,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	while(*s != '\0')
+	while (*s != '\0')
 	{/*evaluate if the character pass is equal to the string character*/
 		if (*s == c)
 			return (s);
+		if (*s == '\0')
+			return (0);
 		s++;
 	}
 	return (0);
