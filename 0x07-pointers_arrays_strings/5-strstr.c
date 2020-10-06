a514e2cb7601e38237d162e6e6a3c03faa48357d
katgzco <katgzco@gmail.com>
2020-10-05 23:34:51 -0500

    change values

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,20 +10,18 @@ char *_strstr(char *haystack, char *needle)
 
 	while (*haystack != '\0')
 	{
-		char *i = haystack;
-		char *j = needle;
 
-		while (*j == *haystack && *j != '\0' && *haystack != '\0')
+		while (*j == *haystack && *needle != '\0' && *haystack != '\0')
 		{
 			haystack++;
-			j++;
+			needle;
 
 		}
-		if (*j == '\0')
+		if (*needle == '\0')
 		{
 			return (i);
 		}
-		haystack = i + 1;
+		haystack += 1;
 
 	}
 	return (0);
