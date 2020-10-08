1adf4b6757f71b21a2e6f631560bc6062116777e
katgzco <katgzco@gmail.com>
2020-10-07 20:41:38 -0500

    Proryect

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _puts_recursion -  prints a string.
+ * s: get an array of string for print.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+		_putchar('\n');
+}
