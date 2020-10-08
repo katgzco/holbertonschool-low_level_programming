72ce3dd714270dbb516d32b89911aff52616b1e8
katgzco <katgzco@gmail.com>
2020-10-07 20:50:52 -0500

    change code

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,10 +5,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_puts_recursion(s + 1);
-		_putchar(*s);
-	}
+	if (*s == '\0')
 		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+
 }
