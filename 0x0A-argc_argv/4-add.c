ccd6f5980af338f8a81925949c289cf71e6ba6f7
katgzco <katgzco@gmail.com>
2020-10-10 19:52:45 -0500

    Change logic

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,16 +13,22 @@ int main(int argc, char *argv[])
 	if (argc < 1)
 		printf("0\n");
 	else
+	{
 		for (i = 1; i < argc; i++)
 		{
-			if (*(*(argv + i)) <= '0' || (*(*(argv + i)) >= '9'))
+			if (*(*(argv + i)) >= '0' && (*(*(argv + i)) <= '9'))
+			{
+				
+				suma += atoi(*(argv + i));
+				printf("%d\n", suma);
+				
+			}
+			else
 			{
 				printf("Error\n");
 				return (1);
 			}
-			suma += atoi(*(argv + i));
 		}
-
-	printf("%d\n", suma);
+	}	
 	return  (0);
 }
