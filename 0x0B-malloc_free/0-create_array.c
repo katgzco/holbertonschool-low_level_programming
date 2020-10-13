b6a62d43ca3bcff8e8e12e44da0736cd27444c0d
katgzco <katgzco@gmail.com>
2020-10-13 16:42:48 -0500

    add char *

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 		return (0);
 	char *p = NULL;
 
-	p = malloc(size * sizeof(char));
+	p =(char *)malloc(size * sizeof(char));
 	if (p == NULL)
 		return (0);
 	for (unsigned int i = 0; i < size; i++)
