d349404370051e72a928a9a0fc6a0a0d899a2eef
katgzco <katgzco@gmail.com>
2020-10-13 16:28:46 -0500

    Tewo task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include"holberton.h"
+/**
+ * create_array - Crate an array and asigned c in memory.
+ * @size: The length of the array.
+ * @c: the character to  asigned in the array.
+ * Return: a pointer to the array in position 0.
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+		return (NULL);
+	char *p = NULL;
+
+	p = malloc(size * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	for (unsigned int i = 0; i < size; i++)
+		*(p + i) = c;
+	return (p);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include"holberton.h"
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ *  which contains a copy of the string given as a parameter.
+ * @str: The length of the array.
+ * Return: return a copy of the parameter.
+ */
+char *_strdup(char *str)
+{
+int lenght = 0;
+int lenght_copy = 0;
+char *p = NULL;
+
+if (str == NULL)
+	return (NULL);
+while (*(str + lenght))
+	lenght++;
+p = malloc(lenght * sizeof(char));
+if (p == NULL)
+	return (NULL);
+for (lenght_copy = 0; lenght >= lenght_copy; lenght_copy++)
+	*(p + lenght_copy) = *(str + lenght_copy);
+return (p);
+}
