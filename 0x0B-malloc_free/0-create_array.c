495b170257878d517cfd74c96ce12952db836bfa
katgzco <katgzco@gmail.com>
2020-10-13 17:00:50 -0500

    Betty

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,18 +12,17 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (0);
-	else
-	{
-		char *p = NULL;
-		unsigned int i = 0;
 
-		p = (char *)malloc(size * sizeof(char));
-		if (p)
-		{
-			for (i = 0; i < size; i++)
-				p[i] = c;
+	char *p = NULL;
+	unsigned int i = 0;
+
+	p = (char *)malloc(size * sizeof(char));
+	if (p)
+	{
+		for (i = 0; i < size; i++)
+			p[i] = c;
 			return (p);
-		}
+	}
 		else
 			return (0);
 	}
