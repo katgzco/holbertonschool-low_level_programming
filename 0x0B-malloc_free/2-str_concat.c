9b07dec755cf05c91e8e950a5a6d30f44ed62eff
katgzco <katgzco@gmail.com>
2020-10-14 12:11:37 -0500

    Add len =0

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,15 +15,21 @@ char *str_concat(char *s1, char *s2)
 		*s1 = '\0';
 		lenght = 0;
 	}
-	for (lenght = 0; *(s1 + lenght); lenght++)
-	;
+	else
+	{
+		for (lenght = 0; *(s1 + lenght); lenght++)
+			;
+	}
 	if (s2 == NULL)
 	{
 		*s2 = '\0';
 		lenght2 = 0;
 	}
-	for (lenght2 = 0; *(s2 + lenght2); lenght2++)
-		;
+	else
+	{
+		for (lenght2 = 0; *(s2 + lenght2); lenght2++)
+			;
+	}
 	lenght += lenght2;
 	p = malloc(lenght * sizeof(char) + 1);
 	if (p == NULL)
