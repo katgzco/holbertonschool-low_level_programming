9ff3c39b8cded25c5cc36ee185057afc6a4bb375
katgzco <katgzco@gmail.com>
2020-10-14 17:51:27 -0500

    nwe logic

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,23 +10,23 @@ char *str_concat(char *s1, char *s2)
 int lenght, lenght2;
 char *p = NULL;
 
+lenght = lenght2 = 0;
 if (s1 == NULL)
 	*s1 = '\0';
-for (lenght = 0; *(s1 + lenght); lenght++)
-	;
 if (s2 == NULL)
 	*s2 = '\0';
+for (lenght = 0; *(s1 + lenght); lenght++)
+	;
 for (lenght2 = 0; *(s2 + lenght2); lenght2++)
 	;
 lenght += lenght2;
-p = malloc((sizeof(char) * lenght)+1);
+p = malloc((lenght * sizeof(char)) + 1);
 if (p == NULL)
 	return (0);
-lenght = 0;
-for (lenght = 0; *s1; lenght++, s1++)
-	*(p + lenght) = *s1;
-for (lenght2 = lenght; *s2; lenght2++, s2++)
-	*(p + lenght2) = *s2;
-*(p + lenght2) = '\0';
+for (lenght = 0; *(s1 + lenght); lenght++)
+	*(p + lenght) = *(s1 + lenght);
+for (lenght2 = 0; *(s2 + lenght2); lenght2++, lenght++)
+	*(p + lenght) = *(s2 + lenght2);
+*(p + lenght) = '\0';
 return (p);
 }
