8c28977cac870e052722423d4cf3526993625fc2
katgzco <katgzco@gmail.com>
2020-10-13 20:44:23 -0500

    2 task

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,29 @@
+#include"holberton.h"
+/**
+ * str_concat- Write a function that concatenates two strings.
+ * @s1: dest string.
+ * @s2: source string.
+ * Return: return a pointer of the concatenate string.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int lenght, lenght2, lenght_copy;
+	char *p = NULL;
+
+	if (s1 == NULL && s2 == NULL)
+		return (0);
+	for (lenght = 0; *(s1 + lenght); lenght++)
+		;
+	for (lenght2 = 0; *(s2 + lenght2); lenght2++)
+		;
+		lenght += lenght2;
+		lenght--;
+	p = (malloc(lenght * sizeof(char)));
+	if (p == NULL)
+		return (0);
+	for (lenght_copy = 0; *s1; lenght_copy++, s1++)
+		*(p + lenght_copy) = *s1;
+	for ( ; *s2; lenght_copy++, s2++)
+		*(p + lenght_copy) = *s2;
+	return (p);
+}
