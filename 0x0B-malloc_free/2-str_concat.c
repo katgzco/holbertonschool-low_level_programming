e67336c4bea1970f738cfc3cc26283ff1f655285
katgzco <katgzco@gmail.com>
2020-10-14 13:59:56 -0500

    Change var

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,34 +11,22 @@ char *str_concat(char *s1, char *s2)
 	char *p = NULL;
 
 	if (s1 == NULL)
-	{
 		*s1 = '\0';
-		lenght = 0;
-	}
-	else
-	{
-		for (lenght = 0; *(s1 + lenght); lenght++)
-			;
-	}
+	for (lenght = 0; *(s1 + lenght); lenght++)
+		;
 	if (s2 == NULL)
-	{
 		*s2 = '\0';
-		lenght2 = 0;
-	}
-	else
-	{
-		for (lenght2 = 0; *(s2 + lenght2); lenght2++)
-			;
-	}
+	for (lenght2 = 0; *(s2 + lenght2); lenght2++)
+		;
 	lenght += lenght2;
 	p = malloc(lenght * sizeof(char) + 1);
 	if (p == NULL)
 		return (0);
-	for (lenght_copy = 0; *s1; lenght_copy++, s1++)
-		*(p + lenght_copy) = *s1;
-	for ( ; *s2; lenght_copy++, s2++)
-		*(p + lenght_copy) = *s2;
-	*(p + lenght_copy) = '\0';
+	lenght = 0;
+	for (lenght = 0; *s1; lenght++, s1++)
+		*(p + lenght) = *s1;
+	for (lenght2 = lenght ; *s2; lenght2++, s2++)
+		*(p + lenght2) = *s2;
+	*(p + lenght2) = '\0';
 	return (p);
 }
-
