532c1df81f6e9544f667d102fefcce0caaf876cd
katgzco <katgzco@gmail.com>
2020-10-14 16:25:36 -0500

    Python

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,26 +7,26 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int lenght, lenght2, lenght_copy;
-	char *p = NULL;
+int lenght, lenght2;
+char *p = NULL;
 
-	if (s1 == NULL)
-		*s1 = '\0';
-	for (lenght = 0; *(s1 + lenght); lenght++)
-		;
-	if (s2 == NULL)
-		*s2 = '\0';
-	for (lenght2 = 0; *(s2 + lenght2); lenght2++)
-		;
-	lenght += lenght2;
-	p = malloc(lenght * sizeof(char) + 1);
-	if (p == NULL)
-		return (0);
-	lenght = 0;
-	for (lenght = 0; *s1; lenght++, s1++)
-		*(p + lenght) = *s1;
-	for (lenght2 = lenght ; *s2; lenght2++, s2++)
-		*(p + lenght2) = *s2;
-	*(p + lenght2) = '\0';
-	return (p);
+if (s1 == NULL)
+	*s1 = '\0';
+for (lenght = 0; *(s1 + lenght); lenght++)
+	;
+if (s2 == NULL)
+	*s2 = '\0';
+for (lenght2 = 0; *(s2 + lenght2); lenght2++)
+	;
+lenght += lenght2;
+p = malloc((sizeof(char) * lenght)+1);
+if (p == NULL)
+	return (0);
+lenght = 0;
+for (lenght = 0; *s1; lenght++, s1++)
+	*(p + lenght) = *s1;
+for (lenght2 = lenght; *s2; lenght2++, s2++)
+	*(p + lenght2) = *s2;
+*(p + lenght2) = '\0';
+return (p);
 }
