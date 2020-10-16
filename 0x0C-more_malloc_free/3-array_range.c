4a5febb91e26dc9b12c46fee113e50c451a69351
katgzco <katgzco@gmail.com>
2020-10-15 19:37:27 -0500

    add if for equal

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,9 +13,11 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (0);
 	size  = max - min;
-	p = malloc(sizeof(int) * size + 1);
+	p = malloc((sizeof(int) * size) + 1);
 	if (!p)
 		return (0);
+	if (max == min)
+		*p = 0;
 	for (size = min ; min <= max; size++, min++)
 		*(p + size) = min;
 	return (p);
