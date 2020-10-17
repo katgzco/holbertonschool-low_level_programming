6878e8c89c5d5e42f868813b4766002457976444
katgzco <katgzco@gmail.com>
2020-10-17 17:07:55 -0500

    Betty

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,28 +7,29 @@
  */
 int **alloc_grid(int width, int height)
 {
-int row, column;
-int **p = NULL;
+	int row, column;
+	int **p = NULL;
 
-if (width <= 0 || height <= 0)
-	return (0);
-p = (malloc(sizeof(int *) * height));
-if (p == NULL)
-	return 0;
-for (row = 0; row < height; row++)
-{
-	p[row] = (malloc(sizeof(int) * width));
-	if (p[row] != NULL)
+	if (width <= 0 || height <= 0)
+		return (0);
+	p = (malloc(sizeof(int *) * height));
+	if (p == NULL)
+		return (0);
+	for (row = 0; row < height; row++)
 	{
-		for (column = 0; column < width;  column++)
-			p[row][column] = 0;
-	}
+		p[row] = (malloc(sizeof(int) * width));
+		if (p[row] != NULL)
+		{
+			for (column = 0; column < width;  column++)
+				p[row][column] = 0;
+		}
 	else
 	{
-	for(row--;row >= 0; row--)
+		for (row--; row >= 0; row--)
 			free(p[row]);
-	free(p);
-	return (0);
+		free(p);
+		return (0);
+	}
 }
 return (p);
 }
