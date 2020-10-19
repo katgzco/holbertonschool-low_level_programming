cd8353962541ecb085628b3dd71bf4c0c8087349
katgzco <katgzco@gmail.com>
2020-10-19 15:28:10 -0500

    Men dont set

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,16 +7,16 @@
  */
 void print_dog(struct dog *d)
 {
-
-	if (d == NULL)
-	exit(0);
-
-	if (d->name == NULL)
-		d->name = "(nil)";
-	if (d-> age < 0)
-		d->age = 0;
-	if (d->owner == NULL)
-		d->owner = "(nil)";
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	if (d)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)");
+		else
+			printf("Name: %s\n", d->name);
+		printf("Age : %f\n", d->age);
+		if (d->owner == NULL)
+			printf("Owner: (nil)");
+		else
+			printf("Owner: %s\n", d->owner);
 	}
 }
