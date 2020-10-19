6b6baafcb6541c97cab1596c5a6de2d2b51ceb09
katgzco <katgzco@gmail.com>
2020-10-19 13:56:46 -0500

    Fix betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,14 +5,13 @@
  * @name:The name of the dog
  * @age: The age of the dog
  * @owner: the owner of the dog.
- * @dog_t: A new type of the structure.
  */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
