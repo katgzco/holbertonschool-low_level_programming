0021cadaab76fcf3e3b81de02b32978bdd08b856
katgzco <katgzco@gmail.com>
2020-10-19 15:20:34 -0500

    2 task

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,14 +7,16 @@
  */
 void print_dog(struct dog *d)
 {
+
 	if (d == NULL)
-		exit(0);
-	else
-	{
-		if (d->name == NULL)
-			d->name = "(nil)";
-		printf("Name: %s\n", d->name);
-		printf("Age : %f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+	exit(0);
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d-> age < 0)
+		d->age = 0;
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
 }
