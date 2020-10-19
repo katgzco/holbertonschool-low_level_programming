ce8dfa9e0e0c30903785a10e23c1c15668195072
katgzco <katgzco@gmail.com>
2020-10-19 13:54:06 -0500

    task new typw

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,13 +6,12 @@
  * @age: The age of the dog
  * @owner: the owner of the dog.
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
+}dog_t;
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
