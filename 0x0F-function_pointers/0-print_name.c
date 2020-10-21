7e13b882e87b34fa466e12995480cac268b07890
katgzco <katgzco@gmail.com>
2020-10-21 15:48:02 -0500

    Change logic

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,12 @@
 #include "function_pointers.h"
 #include <stdlib.h>
 /**
- *print_name - prints a name.
+ * print_name - executes a function given as a parameter on array.
  * @name: a pointer to a string.
  * @f: a pointer to a function.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
+	if (f != NULL)
 		f(name);
 }
