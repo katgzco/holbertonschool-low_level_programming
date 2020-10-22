fecacbfcf40d9f93258e2e47a5bd61ec34d922fc
katgzco <katgzco@gmail.com>
2020-10-22 07:34:36 -0500

    Tasj 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,26 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * main - get the dates for operate the calculator
+ * @argc: the length of the array.
+ * @argv: The array.
+ * Return: 0 - succesful.
+ */
+int main(int argc, char *argv[])
+{
+	int res;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	res = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", res);
+
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,47 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * op_add - function that sub two integers_sub(int a, int b)
+ * @a: a integer from pass to the function.
+ * @b:  a integer from pass to the function.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - function that sub two integers_sub(int a, int b)
+ * @a: a integer from pass to the function.
+ * @b:  a integer from pass to the function.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - function that sub two integers_sub(int a, int b)
+ * @a: a integer from pass to the function.
+ * @b:  a integer from pass to the function.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - function that sub two integers_sub(int a, int b)
+ * @a: a integer from pass to the function.
+ * @b:  a integer from pass to the function.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - function that mod two integers_sub(int a, int b)
+ * @a: a integer from pass to the function.
+ * @b:  a integer from pass to the function.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
