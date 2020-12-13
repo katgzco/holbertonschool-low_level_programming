48eccedb4babc73a3c0868d637dec94efb1ec82b
katgzco <katgzco@gmail.com>
2020-12-13 17:44:29 -0500

    Refactor - change the position of the logic

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,61 +14,28 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *check_return = NULL;
+	dlistint_t *head_ref = NULL, *new_node = NULL;
 
 /*check if is the first element*/
-	if (idx == 0)
-	{
-		check_return = add_dnodeint(h, n);
-		if (check_return != NULL)
-			return (check_return);
-	}
+	if (idx == 0 || *h == NULL)
+		return (add_dnodeint(h, n));
 
-/*creat the node in the between node or in the end*/
-	check_return = add_nodemid(h, n, idx);
-	{
-		if (check_return != NULL)
-			return (check_return);
-	}
-
-	return (NULL);
-}
-
-/**
- * add_nodemid - creates a new node betweent the give position of two nodes.
- * @head: the head of the link list.
- * @n: the value of the member n.
- * @idx: the index of the node.
- * Return: the new node or null.
- */
-
-dlistint_t *add_nodemid(dlistint_t **head, const int n, unsigned int idx)
-{
-	dlistint_t *head_ref = *head, *head_ref_aux = *head, *new_node = NULL;
-	dlistint_t *check_return = NULL;
-
-	head_ref = get_dnodeint_at_index(head_ref, idx);
-	if (head_ref == NULL)
-		return (NULL);
-
-/*check if is the last position, and inserts*/
-	if (head_ref->next == NULL)
-	{
-		check_return = add_dnodeint_end(&head_ref, n);
-		return (check_return);
-	}
-/*if is the middle*/
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
+/*check if is the last element*/
+	if (idx == dlistint_len(*h))
+		return (add_dnodeint_end(h, n));
 
 /*insert between nodes*/
+	new_node = malloc(sizeof(dlistint_t));
+		if (new_node == NULL)
+			return (NULL);
+		new_node->n = n;
+
+	head_ref = get_dnodeint_at_index(*h, idx);
+
 	head_ref = head_ref->prev;
-	head_ref_aux = head_ref->next;
+	head_ref->next->prev = new_node;
+	new_node->next = head_ref->next;
 	head_ref->next = new_node;
-	head_ref_aux->prev = new_node;
-	new_node->next = head_ref_aux;
 	new_node->prev = head_ref;
 
 	return (new_node);
@@ -93,4 +60,25 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		return (head_ref);
 	else
 		return (NULL);
+}
+
+/**
+ * dlistint_len - returns the number of elements in a linked dlistint_t list.
+ * @h: The head of the link list.
+ * Return:  the length of the list.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t length = 0;
+	const dlistint_t *h_reference = h;
+
+	if (h != NULL)
+	{
+		while (h_reference != NULL)
+		{
+			length++;
+			h_reference = h_reference->next;
+		}
+	}
+	return (length);
 }
