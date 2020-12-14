8cdf15373a148fd1c2d9848ebaa2cb5041936b9e
katgzco <katgzco@gmail.com>
2020-12-14 11:33:47 -0500

    Fix edge case

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * get_dnodeint_at_inde - adds a new node at the end of a dlistint_t list.
+ * get_dnodeint_at_index - adds a new node at the end of a dlistint_t list.
  * @head: The head of the link list.
  * @index: theindex of the node to search.
  * Return:  The new node or Null.
@@ -11,11 +11,33 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *head_ref = head;
 	unsigned int counter = 0;
 
+	if (head == NULL || index >= dlistint_len(head))
+		return (NULL);
+
 	for (counter = 0; head_ref->next != NULL && counter < index; counter++)
 		head_ref = head_ref->next;
 
 	if (counter == index)
 		return (head_ref);
 	else
-		return(NULL);
+		return (NULL);
+}
+
+/**
+ * dlistint_len - returns the number of elements in a linked dlistint_t list.
+ * @h: The head of the link list.
+ * Return:  the length of the list.
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t length = 0;
+	const dlistint_t *h_reference = h;
+
+	if (h != NULL)
+	{
+		for (; h_reference != NULL; length++)
+			h_reference = h_reference->next;
+	}
+	return (length);
 }
