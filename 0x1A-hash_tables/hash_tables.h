733adecc69bc52bc900d14e7c7a5f6267e5d9f63
katgzco <katgzco@gmail.com>
2021-01-15 20:42:43 -0500

    Feature - five upload - add library actualizate

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdio.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -41,5 +42,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+
+/*own functions*/
+int update_node(hash_node_t *array_index, const char *key, const char *value);
 
 #endif /*hash_tables */
