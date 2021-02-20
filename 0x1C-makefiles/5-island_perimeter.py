cb1f09876c262e84d68fdc74b6e91d01c11f290c
katgzco <katgzco@gmail.com>
2021-02-19 22:46:13 -0500

    Fix - change name of the function and logic

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-def isla(grid):
+""" Island Perimeter """
+
+
+def island_perimeter(grid):
     """ returns the perimeter of the island described in grid
 
     Args:
@@ -8,27 +11,33 @@ def isla(grid):
     Returns:
         [int]: [perimeter of the island described in grid]
     """
-    size = len(grid[0]) - 1
     column = 0
     row = 0
     counter = 0
-    resul = 0
 
-    if not grid or size == -1:
+    if not grid or len(grid) == 0:
         return 0
 
-    for row in range(size):
-        for column in range(size):
-            if grid[row][column] == 1:
-                counter = 4
-                if column >= 1 and grid[row][column + 1] == 1:
-                    counter -= 1
-                if column < (size - 1) and grid[row][column - 1] == 1:
-                    counter -= 1
-                if row >= 1 and grid[row - 1][column] == 1:
-                    counter -= 1
-                if row < (size - 1) and grid[row + 1][column + 1] == 1:
-                    counter -= 1
-                resul += counter
+    for row in range(len(grid)):
+        for column in range(len(grid[row])):
+            if column >= 1 and grid[row][column] == 1:
+                if grid[row][column - 1] == 0:
+                    counter += 1
+                if grid[row][column + 1] == 0:
+                    counter += 1
+                if grid[row - 1][column] == 0:
+                    counter += 1
+                if grid[row + 1][column] == 0:
+                    counter += 1
 
-    return (resul - 1)
+    return (counter)
+
+
+grid = [
+    [0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+]
+print(island_perimeter(grid))
