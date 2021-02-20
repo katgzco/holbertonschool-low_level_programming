024eb2e0c875b01fd5a6da6d7f5d06dcc8a96973
katgzco <katgzco@gmail.com>
2021-02-19 23:58:06 -0500

    Fix -  feauture

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
-""" Island Perimeter """
+"""module island perimter"""
 
 
 def island_perimeter(grid):
-    """ returns the perimeter of the island described in grid
-    Args:
-        grid ([list]): [list of list of integers]
-    Returns:
-        [int]: [perimeter of the island described in grid]
     """
-
-
-rows, columns, perimeter = len(grid), len(grid[0]), 0
-for row in range(rows):
-    for spot in range(columns):
-        if grid[row][spot] == 0:
-            continue
-        if row == 0 or grid[row - 1][spot] == 0:
-            perimeter += 1
-        if row == rows - 1 or grid[row + 1][spot] == 0:
-            perimeter += 1
-        if spot == 0 or grid[row][spot - 1] == 0:
-            perimeter += 1
-        if spot == columns - 1 or grid[row][spot + 1] == 0:
-            perimeter += 1
-return perimeter
+    Function returns the perimeter
+    of the island described in grid:
+    grid is a list of list of integers:
+    0 represents a water zone
+    1 represents a land zone
+    One cell is a square with side length 1
+    Grid cells are connected horizontally/vertically
+    (not diagonally).
+    Grid is rectangular, width and height
+    don’t exceed 100
+    Grid is completely surrounded by water, and
+    there is one island (or nothing).
+    """
+    rows, columns, perimeter = len(grid), len(grid[0]), 0
+    for row in range(rows):
+        for spot in range(columns):
+            if grid[row][spot] == 0:
+                continue
+            if row == 0 or grid[row - 1][spot] == 0:
+                perimeter += 1
+            if row == rows - 1 or grid[row + 1][spot] == 0:
+                perimeter += 1
+            if spot == 0 or grid[row][spot - 1] == 0:
+                perimeter += 1
+            if spot == columns - 1 or grid[row][spot + 1] == 0:
+                perimeter += 1
+    return perimeter
